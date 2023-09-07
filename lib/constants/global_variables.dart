ad5458a11a2151657a2067c630cb8c74330e58ba
VihaoHT <haoanna1232@gmail.com>
2023-09-07 16:27:20 +0700

    update

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = 'http://192.168.2.6:3000';
+String uri = 'http://youripv4address:3000';
 
 class GlobalVariables {
   // COLORS
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
-const DB = "mongodb+srv://haoanna1232:Vihao1235@cluster0.mbmsav6.mongodb.net/?retryWrites=true&w=majority";
+const DB = "mongodb+srv://<username>:<password>@cluster0.mbmsav6.mongodb.net/?retryWrites=true&w=majority";
 
 const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/admin");
